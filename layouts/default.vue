185c851751ceebf2f81eb6578c763636726b5e65
Claudio Mendonca <claudioccm@gmail.com>
2025-06-05 05:52:19 -0700

    layout

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <ccm-master-grid>
+    <ccm-topbar id="topbar" />
+    <ccm-hero id="hero" />
+    <slot />
+    <ccm-footer id="footer" />
+  </ccm-master-grid>
+</template>
+
+<style>
+#topbar {
+  grid-row: 1 / 2;
+  z-index: 2;
+}
+
+#hero {
+  grid-row: 1 / 5;
+  z-index: 1;
+}
+
+</style> 
