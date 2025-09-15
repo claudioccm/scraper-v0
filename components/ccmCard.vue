877854b65d17bb48d41b107a7f6e0aea159e1c54
Claudio Mendonca <claudioccm@gmail.com>
2025-09-15 11:29:22 -0700

    accessible card

--- components/ccmCard.vue
+++ components/ccmCard.vue
@@ -1,25 +1,26 @@
 <template>
- 
-  <component
-    :is="isLink ? 'NuxtLink' : 'div'"
-    class="ccm-card"
-    v-bind="wrapperAttrs"
+  <nuxt-link 
+    class="ccm-card" 
+    :to="to" 
+    :aria-label="title" 
+    :aria-describedby="ctaId" 
+    :image="image"
     :style="{
       '--_card-padding': `var(--space-${size})`,
       '--_card-background-color': `var(--${backgroundColor})`
     }"
     >
-  
-    <div class="ccm-card__text">
-      <slot />
-      <span v-if="isLink" :id="ctaId" class="visually-hidden" aria-hidden="true">{{ ctaText }}</span>
-    </div>
+    
+  <slot name="image">
+    <img class="ccm-card__image" v-if="image" :src="image" :alt="title" />
+    <div v-else class="ccm-card__image"></div>
+  </slot>
 
-    <slot name="image">
-      <img class="ccm-card__image" v-if="image" :src="image" :alt="imageAlt || ''" />
-      <div v-else class="ccm-card__image" aria-hidden="true"></div>
-    </slot>
-  </component>
+  <div class="ccm-card__text">
+    <slot />
+  </div>
+    
+  </nuxt-link>
 </template>
 
 <script setup>
@@ -38,10 +39,6 @@ const props = defineProps({
     type: String,
     required: false
   },
-  imageAlt: {
-    type: String,
-    required: false
-  },
   size: {
     type: String,
     default: 'l'
@@ -49,10 +46,6 @@ const props = defineProps({
   backgroundColor: {
     type: String,
     default: 'color-primary-tint-20'
-  },
-  ctaText: {
-    type: String,
-    default: 'Read more'
   }
 })
 
@@ -82,15 +75,14 @@ const wrapperAttrs = computed(() => {
 </script>
 
 <style scoped>
+
 .ccm-card {
   --_card-padding: var(--space-l);
   --_card-border-radius: var(--border-radius-l);
   --_card-gap: var(--space-m);
   --_card-background-color: var(--color-primary-tint-20);
-  --_card-border-width: 2px;
-  --_card-border-style: solid;
-  --_card-color: var(--color-primary-tint-80);
 }
+
 
 .ccm-card {
   display: flex;
@@ -104,11 +96,11 @@ const wrapperAttrs = computed(() => {
 
 .ccm-card__text {
   flex: 1;
-  padding: 0 var(--_card-padding) var(--_card-padding);
+  padding-block: var(--_card-padding);
 }
 
 .ccm-card__image {
-  order: -1;
+  flex: 1;
   padding: var(--_card-padding);
   background-color: var(--_card-background-color);
   width: 100%;
@@ -116,17 +108,23 @@ const wrapperAttrs = computed(() => {
   border-radius: var(--_card-border-radius);
 }
 
-.visually-hidden {
-  position: absolute;
-  width: 1px;
-  height: 1px;
-  padding: 0;
-  margin: -1px;
-  overflow: hidden;
-  clip: rect(0 0 0 0);
-  clip-path: inset(50%);
-  white-space: nowrap;
-  border: 0;
+
+
+@media (min-width: 768px) {
+  .ccm-card { flex-direction: row; }
+
+  .ccm-card__text { padding-inline: var(--_card-padding); }
+
+  .ccm-card__image {
+    width: 100%;
+    aspect-ratio: 1/1; 
+  }
+
+  .ccm-card:nth-child(even) { 
+    flex-direction: row-reverse; 
+    .ccm-card__text { text-align: right; }
+  }
+  
 }
 
 </style>
